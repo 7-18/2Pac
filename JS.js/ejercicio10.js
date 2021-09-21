abbb3d5742578aced75dda2ae272e335a39494f2
7-18 <kevinbrianbr@gmail.com>
2021-09-21 18:56:43 -0500

    segundo commit

---
+++ JS.js/ejercicio10.js
@@ -0,0 +1,25 @@
+const nombreAlbum1 = () => {
+   alert("Éste es el álbum debut '2Pacalypse Now', lanzado el 12 de noviembre de 1991.");
+}
+
+const nombreAlbum2 = () => {
+   alert("Éste es el segundo álbum 'Strictly 4 My N.I.G.G.A.Z.', lanzado el 16 de febrero de 1993.");
+}
+
+const nombreAlbum3 = () => {  
+    alert("Éste es el tercer álbum 'Me Against the World', lanzado el 14 de marzo de 1995.");
+}
+
+
+const nombreAlbum4 = () => {
+    alert("Éste es el cuarto álbum 'All Eyez on Me', lanzado el 13 de febrero de 1996.");
+}
+
+const nombreAlbum5 = () => {
+    alert("Éste es el primer álbum póstumo 'The Don Killuminati: The 7 Day Theory', lanzado el 5 de noviembre de 1996.");
+}
+
+const nombreAlbum6 = () => {
+    alert("Éste es el segundo póstumo álbum 'R U Still Down?', lanzado el 25 de noviembre de 1997.");
+}
+
